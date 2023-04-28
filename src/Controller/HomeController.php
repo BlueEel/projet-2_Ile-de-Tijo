4b7c3f655401ae84ef2a7e7adf6e1a3ee384dcc4
Arthur Gaychet <arthur.gaychet81@gmail.com>
2023-04-28 10:28:06 +0200

    Ajout d'infos pratiques

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,9 +4,6 @@ namespace App\Controller;
 
 class HomeController extends AbstractController
 {
-    /**
-     * Display home page
-     */
     public function index(): string
     {
         return $this->twig->render('Home/index.html.twig');
@@ -16,5 +13,4 @@ class HomeController extends AbstractController
     {
         return $this->twig->render('Home/index.html.twig');
     }
-
 }

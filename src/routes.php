70f906b16f1a705b8ab02235044ab182c4b4ba29
AMichelas <120266869+AMichelas@users.noreply.github.com>
2023-04-28 09:50:35 +0200

    Ajout du Snack et Jeux

--- src/routes.php
+++ src/routes.php
@@ -14,4 +14,5 @@ return [
     'items/add' => ['ItemController', 'add',],
     'items/delete' => ['ItemController', 'delete',],
     'snack' => ['SnackController', 'index',],
+    'jeux' => ['JeuxController', 'index',],
 ];

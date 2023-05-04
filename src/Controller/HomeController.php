1be0810a2fdf94dbd49fae85e1abce944442ef6f
BlueEel <baptiste.chasset@gmail.coml>
2023-05-04 12:30:03 +0200

    footer modif partie trois

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,10 +4,12 @@ namespace App\Controller;
 
 class HomeController extends AbstractController
 {
-    /**
-     * Display home page
-     */
     public function index(): string
+    {
+        return $this->twig->render('Home/index.html.twig');
+    }
+
+    public function notFound(): string
     {
         return $this->twig->render('Home/index.html.twig');
     }

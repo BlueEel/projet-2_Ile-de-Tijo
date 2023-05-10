983db6fb0ef99293cb9f69947e2132cf9644cb93
AMichelas <120266869+AMichelas@users.noreply.github.com>
2023-05-10 12:35:41 +0200

    Ajout page + bouton accessibilité snack

--- src/routes.php
+++ src/routes.php
@@ -18,4 +18,5 @@ return [
     // Birthday
     'anniversaire' => ['BirthdayController', 'index',],
     'galerie' => ['GalerieController', 'index',],
+    'snack/formule' => ['FormuleController', 'index',],
 ];
